0f4e9a971b7b4046328c1afe85d490ea3ffb53a5
rkatsu710 <rkatsube1919@gmail.com>
2025-08-23 16:51:54 +0900

    Fix lualine statusline not showing
    - Add laststatus=3 to options.lua to ensure statusline is always visible
    - Update lualine config to use opts instead of config function
    - Set globalstatus=true for better multi-window experience
    - Add lazy extension to lualine extensions
    - Use vim.g.have_nerd_font for icons_enabled setting
    
    This should resolve the issue where lualine statusline was not appearing.

--- lua/options.lua
+++ lua/options.lua
@@ -15,6 +15,9 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- Always show statusline
+vim.o.laststatus = 3
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
